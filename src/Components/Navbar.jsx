455d04b308564fcb8363ad65acfc80008acad8a9
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-24 00:11:08 +0300

    covid news components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Transition } from '@headlessui/react'
 // import {Link} from 'react-router-dom'
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
       <nav className="bg-gray-800">
@@ -30,11 +32,13 @@ function Navbar() {
             </div>
             <div className="-mr-2 flex md:hidden">
               <button type='button'
+              onClick={() =>setIsOpen(!isOpen)}
               className='bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
               aria-controls='mobile-menu'
               aria-expanded='false'
               >
                 <span className='sr-only'>Open main menu</span>
+                {!isOpen ?(
                 <svg
                 className='block h-6 w-6'
                 xmlns='http://www.w3.org/2000/svg'
@@ -50,6 +54,7 @@ function Navbar() {
                   d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
+                ) : (
                 <svg
                   className="hidden h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -65,12 +70,23 @@ function Navbar() {
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
+                )}
               </button>
             </div>
           </div>
         </div>
         {/* for mobile view */}
-        <div className='md:hidden' id='mobile-menu'>
+        <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+        >
+          {(ref)=>(
+                    <div className='md:hidden' id='mobile-menu'>
           <div className='px-2 pt-2 space-y-1 sm:px-3'>
           <a href="/" className='hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium'>
                     Covid
@@ -83,6 +99,9 @@ function Navbar() {
                   </a>
           </div>
         </div>
+          )}
+        </Transition>
+
       </nav>
 
       <main>
