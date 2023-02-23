21eceab30b00ea69c3cd4b2c4da1248bf948fd63
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-24 01:21:17 +0300

    rapid api redux api consumption

---
+++ src/services/CovidNews.js
@@ -0,0 +1,15 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const covidApi = createApi({
+    reducerPath: 'covidApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+        prepareHeaders: (Headers) => {
+            Headers.set('X-RapidAPI-Key','71375eced9msh7c6b37227e6be82p17f352jsn2dd1978a502b')
+            return Headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getNews: builder.query({ query : () => '/api/news/get-coronavirus-news/0'})
+    })
+})
